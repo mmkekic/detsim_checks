fb42e7ea7335f6af4532fe8f81f59a6e1e872a7e
mmkekic <mmkekic@gmail.com>
2020-09-29 12:59:53 +0200

    Add numpy include

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-
+import numpy
 from Cython.Compiler.Options import get_directive_defaults
 import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
@@ -10,4 +10,4 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 setup(
-    ext_modules = cythonize(Extension("*", sources=["*.pyx"], extra_compile_args=["-O3"], define_macros=[('CYTHON_TRACE', '1')])))
+    ext_modules = cythonize(Extension("*", sources=["*.pyx"], include_dirs=[numpy.get_include()], extra_compile_args=["-O3"], define_macros=[('CYTHON_TRACE', '1')])))
